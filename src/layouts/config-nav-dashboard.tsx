add9a004b8deea5d2ed22948def3376e204bad6e
Yisus <jesusclash111@gmail.com>
2024-06-28 11:39:14 -0400

    First commit

---
+++ src/layouts/config-nav-dashboard.tsx
@@ -0,0 +1,103 @@
+import { paths } from 'src/routes/paths';
+
+import { CONFIG } from 'src/config-global';
+
+import { SvgColor } from 'src/components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name: string) => (
+  <SvgColor src={`${CONFIG.site.basePath}/assets/icons/navbar/${name}.svg`} />
+);
+
+const ICONS = {
+  job: icon('ic-job'),
+  blog: icon('ic-blog'),
+  chat: icon('ic-chat'),
+  mail: icon('ic-mail'),
+  user: icon('ic-user'),
+  file: icon('ic-file'),
+  lock: icon('ic-lock'),
+  tour: icon('ic-tour'),
+  order: icon('ic-order'),
+  label: icon('ic-label'),
+  blank: icon('ic-blank'),
+  kanban: icon('ic-kanban'),
+  folder: icon('ic-folder'),
+  course: icon('ic-course'),
+  banking: icon('ic-banking'),
+  booking: icon('ic-booking'),
+  invoice: icon('ic-invoice'),
+  product: icon('ic-product'),
+  calendar: icon('ic-calendar'),
+  disabled: icon('ic-disabled'),
+  external: icon('ic-external'),
+  menuItem: icon('ic-menu-item'),
+  ecommerce: icon('ic-ecommerce'),
+  analytics: icon('ic-analytics'),
+  dashboard: icon('ic-dashboard'),
+  parameter: icon('ic-parameter'),
+};
+
+// ----------------------------------------------------------------------
+
+export const navData = [
+  /**
+   * Overview
+   */
+  {
+    items: [
+      { title: 'Dashboard', path: paths.dashboard.root, icon: ICONS.dashboard },
+      { title: 'Calendario', path: paths.dashboard.two, icon: ICONS.calendar },
+      { title: 'Chat', path: paths.dashboard.three, icon: ICONS.chat },
+    ],
+  },
+  /**
+   * Management
+   */
+  {
+    subheader: 'Mantenimiento',
+    items: [
+      {
+        title: 'Afiliados',
+        path: paths.dashboard.group.root,
+        icon: ICONS.job,
+        children: [
+          { title: 'Four', path: paths.dashboard.group.root },
+          { title: 'Five', path: paths.dashboard.group.five },
+          { title: 'Six', path: paths.dashboard.group.six },
+        ],
+      },
+      {
+        title: 'Cobros',
+        path: paths.dashboard.group.root,
+        icon: ICONS.banking,
+        children: [
+          { title: 'Four', path: paths.dashboard.group.root },
+          { title: 'Five', path: paths.dashboard.group.five },
+          { title: 'Six', path: paths.dashboard.group.six },
+        ],
+      },
+      {
+        title: 'Ingresos y Egresos',
+        path: paths.dashboard.group.root,
+        icon: ICONS.ecommerce,
+        children: [
+          { title: 'Four', path: paths.dashboard.group.root },
+          { title: 'Five', path: paths.dashboard.group.five },
+          { title: 'Six', path: paths.dashboard.group.six },
+        ],
+      },
+      {
+        title: 'Usuarios',
+        path: paths.dashboard.group.root,
+        icon: ICONS.user,
+        children: [
+          { title: 'Four', path: paths.dashboard.group.root },
+          { title: 'Five', path: paths.dashboard.group.five },
+          { title: 'Six', path: paths.dashboard.group.six },
+        ],
+      },
+    ],
+  },
+];
